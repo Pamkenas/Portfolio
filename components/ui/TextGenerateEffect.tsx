cffd33d1ab5f09c0c87d0dd39789faad066b1013
Pamkenas <kimbiras@gmail.com>
2025-01-18 03:14:17 +0200

    initial commit

--- components/ui/TextGenerateEffect.tsx
+++ components/ui/TextGenerateEffect.tsx
@@ -9,13 +9,13 @@ export const TextGenerateEffect = ({
   filter = true,
   duration = 0.5,
 }: {
-  words: string;
+  words: string[];
   className?: string;
   filter?: boolean;
   duration?: number;
 }) => {
   const [scope, animate] = useAnimate();
-  let wordsArray = words.split(" ");
+
   useEffect(() => {
     animate(
       "span",
@@ -24,36 +24,41 @@ export const TextGenerateEffect = ({
         filter: filter ? "blur(0px)" : "none",
       },
       {
-        duration: duration ? duration : 1,
+        duration: duration,
         delay: stagger(0.2),
       }
     );
-  }, [scope.current]);
+  }, [animate, filter, duration]);
 
   const renderWords = () => {
     return (
       <motion.div ref={scope}>
-        {wordsArray.map((word, idx) => {
-          return (
-            <motion.span
-              key={word + idx}
-              className="dark:text-white text-black opacity-0"
-              style={{
-                filter: filter ? "blur(10px)" : "none",
-              }}
-            >
-              {word}{" "}
-            </motion.span>
-          );
-        })}
+        {words.map((line, idx) => (
+          <div key={idx} className="block text-center">
+            {line.split(" ").map((word, wordIdx) => (
+              <motion.span
+                key={`${word}-${wordIdx}`}
+                className={`${
+                  word === "Full-Stack" ||
+                  word === "Jr." ||
+                  word === "Developer"
+                    ? "text-purple"
+                    : "dark:text-white text-black"
+                } opacity-0`}
+              >
+                {word}{" "}
+              </motion.span>
+            ))}
+          </div>
+        ))}
       </motion.div>
     );
   };
 
   return (
     <div className={cn("font-bold", className)}>
-      <div className="mt-4">
-        <div className=" dark:text-white text-black text-2xl leading-snug tracking-wide">
+      <div className="my-4">
+        <div className="dark:text-white text-black leading-snug tracking-wide">
           {renderWords()}
         </div>
       </div>
